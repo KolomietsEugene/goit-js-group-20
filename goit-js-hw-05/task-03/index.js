38ce02328ab31c60a29c3ab0971ff59b3256c14c
Eugene Kolomiiets <kolomiets1990@gmail.com>
2020-05-15 09:52:41 +0300

    HW 5 (without ES5 constructors)

---
+++ goit-js-hw-05/task-03/index.js
@@ -0,0 +1,36 @@
+class Storage {
+  constructor(incomeItems = []) {
+    this.items = incomeItems;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(items = []) {
+    this.items.push(items);
+  }
+
+  removeItem(item) {
+    while (this.items.includes(item)) {
+      const index = this.items.indexOf(item);
+      this.items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
